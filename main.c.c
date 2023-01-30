4f223c38eda5cdc2cf01a9d68e1741ec74d386d7
obaida-hamza <119443728+obaida-hamza@users.noreply.github.com>
2023-01-30 13:20:00 +0200

    Update main.c.c

--- main.c.c
+++ main.c.c
@@ -21,134 +21,134 @@ int main()
 		int i;
 		while(i<9)
 		{
-			if (i < 8&& green_button ==1)
+			if (i < 8&& green_button =1)
 			{
 				i = i+1;	
 			}
-			else if (i >= 8&& green_button ==1)
+			else if (i >= 8&& green_button =1)
 			{
 				i = 0;	
 			}
-			else if (i >= 8&& red_button ==1)
+			else if (i >= 8&& red_button =1)
 			{
 				i = i-1;	
 			}
-			else if (i <= 0&& red_button ==1)
+			else if (i <= 0&& red_button =1)
 			{
 				i = 8;
 			}
 			switch (i)
 			{
 				case 0:
-				a == high ;
-				b == high ;
-				c == high ;
-				d == high ;
-				e == high  ;
-				f == high ;
-				g == low ;
-				d == high ;
+				a = high ;
+				b = high ;
+				c = high ;
+				d = high ;
+				e = high  ;
+				f = high ;
+				g = low ;
+				d = high ;
 				break;
 				
 				case 1 :
-				a == low;
-				b == high;
-				c == high;
-				d == low;	
-				e == low;	
-				f == low;
-				g == low;
-				p == low;
+				a = low;
+				b = high;
+				c = high;
+				d = low;	
+				e = low;	
+				f = low;
+				g = low;
+				p = low;
 				break;
 				case 2 :
-				a == high;
-				b == high;
-				c == low;
-				d == high;
-				e == high;
-				f == low;
-				g == high;
-				h == high;
-				d == high;
+				a = high;
+				b = high;
+				c = low;
+				d = high;
+				e = high;
+				f = low;
+				g = high;
+				h = high;
+				d = high;
 				break;
 				case 3 :
-				a == high;
-				b == high;
-				c == high;
-				d == high;
-				e == low;
-				f == low;
-				g == high;
-				d == high;
-				break;
+				a = high;
+				b = high;
+				c = high;
+				d = high;
+				e = low;
+				f = low;
+				g = high;
+				d = high;
+				brak;
 				case 4:
-				a == low ;
-				b == high ;
-				c == high ;
-				d == low;
-				e == low;
-				f == high;
-				g == high ; 
+				a = low ;
+				b = high ;
+				c = high ;
+				d = low;
+				e = low;
+				f = high;
+				g = high ; 
 				break;
 				case 5:
-				a == high;
-				b == low;
-				c == high;
-				d == high;
-				e == low;
-				f == high;
-				g == high;
-				d == high;
+				a = high;
+				b = low;
+				c = high;
+				d = high;
+				e = low;
+				f = high;
+				g = high;
+				d = high;
 				break;
 				case 6:
-				a == high ;
-				b == low ;
-				c == high ;
-				d == high ;
-				e == high ;
-				f == high ;
-				g == high ;
-				d == high ;
+				a = high ;
+				b = low ;
+				c = high ;
+				d = high ;
+				e = high ;
+				f = high ;
+				g = high ;
+				d = high ;
 				break;
 				case 7:
-				a == high ;
-				b == high ;
-				c == high ;
-				d == low  ;
-				e == low  ;
-				f == low  ;
-				g == low  ;
-				d == low  ;
+				a = high ;
+				b = high ;
+				c = high ;
+				d = low  ;
+				e = low  ;
+				f = low  ;
+				g = low  ;
+				d = low  ;
 				break;
 				case 6:
-				a == high ;
-				b == low ;
-				c == high ;
-				d == high ;
-				e == high ;
-				f == high ;
-				g == high ;
-				d == high ;
+				a = high ;
+				b = low ;
+				c = high ;
+				d = high ;
+				e = high ;
+				f = high ;
+				g = high ;
+				d = high ;
 				break;
 				case 7:
-				a == high ;
-				b == high ;
-				c == high ;
-				d == low  ;
-				e == low  ;
-				f == low  ;
-				g == low  ;
-				d == low  ;
+				a = high ;
+				b = high ;
+				c = high ;
+				d = low  ;
+				e = low  ;
+				f = low  ;
+				g = low  ;
+				d = low  ;
 				break;
 				case 8:
-				a == high ;
-				b == high ;
-				c == high ;
-				d == high ;
-				e == high ;
-				f == high ;
-				g == high ;
-				d == high ;
+				a = high ;
+				b = high ;
+				c = high ;
+				d = high ;
+				e = high ;
+				f = high ;
+				g = high ;
+				d = high ;
 				break;
 				
 			}
